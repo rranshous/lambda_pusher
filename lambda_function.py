b4fe8fa93a763d53d39b8a6aaa524bb44af4b669
Robby Ranshous <rranshous@gmail.com>
2016-02-02 23:14:53 -0500

    working

--- lambda_function.py
+++ lambda_function.py
@@ -20,12 +20,13 @@ def lambda_handler(event, context):
     for record in event['Records']:
         # Kinesis data is base64 encoded so decode here
         payload_string = base64.b64decode(record['kinesis']['data'])
-        print("Decoded payload: " + payload)
+        print("Decoded payload: " + payload_string)
         event_package = json.loads(payload_string)
+        print("event_package: %s [%s]" % (event_package, type(event_package)))
         target_url = event_package['target_url']
         event_data = event_package['event']
         event_data_string = json.dumps(event_data)
         print("target_url: %s; event_data: %s" % (target_url, event_data_string));
-        response = manager.request('POST', target_url, event_data_string)
+        response = manager.request('POST', target_url, body=event_data_string)
         print("response: %s" % response)
     return 'Successfully processed {} records.'.format(len(event['Records']))
